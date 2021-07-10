081515c1b539292ef0331f5efe54b05dde12cbec
James Bielicke <j.bielicke@ufl.edu>
2021-07-09 22:13:47 -0500

    ADD reworked & tested the cookie & localstorage inputs

--- index.js
+++ index.js
@@ -3,25 +3,36 @@ var nameSpan = document.querySelector('span')
 var formEl = document.querySelector('form')
 var clear = document.querySelector('#clear')
 var textarea = document.querySelector('textarea')
-var notesarea = document.querySelector('notes-area')
+//var notesarea = document.querySelector('notes-area')
 
 // Retrieve name and note content from cookies and localstorage
 // Then apply them to elements on the page
 // YOUR CODE HERE
 
-localStorage.setItem("NAMEHEADER", "Jim")
-//document.getElementById("nameid").innerHTML = nameHeader;
-document.getElementById("nameid").innerHTML = localStorage.getItem("NAMEHEADER");
+  document.cookie = inputform
+  var inputform = nameSpan.textContent
 
-localStorage.setItem("TEXT-AREA", "Please add something here")
-document.getElementById("notes-area").innerHTML = localStorage.getItem("TEXT-AREA");
+  formEl.onsubmit = function(e) {
 
-formEl.onsubmit = function(e) {
-  // prevents form submission
-  e.preventDefault()
-  // save name element's content to cookies
-  // save textarea's content to localstorage
-  // YOUR CODE HERE
+// prevents form submission
+e.preventDefault()
+// save name element's content to cookies
+// save textarea's content to localstorage
+// YOUR CODE HERE
+
+  document.cookie = 'name=span'
+  
+  localStorage.setItem('form', 'textarea')
+
+
+
+
+  
+  /*localStorage.setItem("NAMEHEADER", "Jim")
+  document.getElementById("nameid").innerHTML = localStorage.getItem("NAMEHEADER");
+
+  localStorage.setItem("TEXT-AREA", "Please add something here")
+  document.getElementById("notes-area").innerHTML = localStorage.getItem("TEXT-AREA");*/
 
 
 
